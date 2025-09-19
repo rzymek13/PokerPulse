c7767df793bc7fbbebf7285418d59bd70b314d7c
Rzymek <przemek.rzymkowski@gmail.com>
2025-09-19 20:49:43 +0200

    key-vault

--- src/test/java/prtech/com/pokerpulse/service/PlayerServiceTest.java
+++ src/test/java/prtech/com/pokerpulse/service/PlayerServiceTest.java
@@ -1,27 +1,9 @@
 package prtech.com.pokerpulse.service;
 
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.transaction.annotation.Transactional;
-import prtech.com.pokerpulse.model.player.Player;
-import prtech.com.pokerpulse.repository.PlayerRepository;
 
-//@Transactional
 class PlayerServiceTest {
 
-    PlayerRepository repository;
-
-    @Test
-    public void createUpdateDeleteOrder() {
-        System.out.println(repository.countItems());
-        Player player = new Player("testUser", "testPass");
-        Player player2 = new Player("testUser2", "testPass2");
-        repository.save(player);
-        repository.save(player2);
-        System.out.println(repository.countItems());
-    }
     }
 
 
