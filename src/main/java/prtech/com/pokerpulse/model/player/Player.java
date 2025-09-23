a80497133a959e53872380aa9acbc389b0502b17
Rzymek <przemek.rzymkowski@gmail.com>
2025-09-23 17:50:41 +0200

    docker db + testcontainers works

--- src/main/java/prtech/com/pokerpulse/model/player/Player.java
+++ src/main/java/prtech/com/pokerpulse/model/player/Player.java
@@ -1,6 +1,5 @@
 package prtech.com.pokerpulse.model.player;
 
-
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.annotation.Id;
@@ -21,7 +20,6 @@ public class Player {
     private String password;
 //    private boolean ready = false;
 //    private List<Card> hand = new ArrayList<>();
-
 
     public Player(String username, String password) {
         this.username = username;
